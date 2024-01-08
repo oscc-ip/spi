fd8eeb305d50a86fa244b87f15c1fbadb305df4c
maksyuki <maksyuki@126.com>
2024-01-08 09:43:14 +0800

    feat: rewrite all spi module

--- rtl/apb4_spi.sv
+++ rtl/apb4_spi.sv
@@ -17,51 +17,85 @@ module apb4_spi (
 );
 
   logic [3:0] s_apb4_addr;
-  logic [31:0] s_spi_ctrl_d, s_spi_ctrl_q;
-  logic [31:0] s_spi_tx_d, s_spi_tx_q;
-  logic [31:0] s_spi_rx_q;
-  logic [ 7:0] s_spi_div;
-  logic [ 3:0] s_spi_nss;
-  logic [ 1:0] s_spi_dtb;
-  logic [31:0] s_spi_rv_rx;
-  logic s_spi_busy, s_spi_st, s_spi_en, s_spi_rxe, s_spi_txe;
-  logic s_spi_mstr, s_spi_rdm, s_spi_ass, s_spi_lsb, s_spi_cpol, s_spi_cpha;
+  logic s_apb4_wr_hdshk, s_apb4_rd_hdshk;
+  logic [`SPI_CTRL1_WIDTH-1:0] s_spi_ctrl1_d, s_spi_ctrl1_q;
+  logic s_spi_ctrl1_en;
+  logic [`SPI_CTRL2_WIDTH-1:0] s_spi_ctrl2_d, s_spi_ctrl2_q;
+  logic s_spi_ctrl2_en;
+  logic [`SPI_DIV_WIDTH-1:0] s_spi_div_d, s_spi_div_q;
+  logic s_spi_div_en;
+  logic [`SPI_TXR_WIDTH-1:0] s_spi_txr_d, s_spi_txr_q;
+  logic s_spi_txr_en;
+  logic [`SPI_RXR_WIDTH-1:0] s_spi_rxr_d, s_spi_rxr_q;
+  logic s_spi_rxr_en;
+  logic [`SPI_STAT_WIDTH-1:0] s_spi_stat_d, s_spi_stat_q;
+  logic s_spi_stat_en;
 
-  assign s_apb4_addr = apb4.paddr[5:2];
+  logic s_bit_cpha, s_bit_cpol, s_bit_lsb, s_bit_ass, s_bit_rdm, s_bit_sstr;
+  logic [1:0] s_bit_dtb;
+  logic s_bit_txie, s_bit_rxie, s_bit_en, s_bit_st;
+  logic [3:0] s_bit_nss;
+  logic s_bit_txif, s_bit_rxif, s_bit_busy;
+
+  assign s_apb4_addr     = apb4.paddr[5:2];
   assign s_apb4_wr_hdshk = apb4.psel && apb4.penable && apb4.pwrite;
   assign s_apb4_rd_hdshk = apb4.psel && apb4.penable && (~apb4.pwrite);
-  assign apb4.pready = 1'b1;
-  assign apb4.pslverr = 1'b0;
-  assign irq_o = s_spi_en && (s_spi_txe || s_spi_rxe);
-  assign s_spi_rv_rx = {s_spi_rx_q[7:0], s_spi_rx_q[15:8], s_spi_rx_q[23:16], s_spi_rx_q[31:24]};
-  assign s_spi_nss = s_spi_ctrl_q[24:21];
-  assign s_spi_busy = s_spi_ctrl_q[20];
-  assign s_spi_st = s_spi_ctrl_q[19];
-  assign s_spi_en = s_spi_ctrl_q[18];
-  assign s_spi_rxe = s_spi_ctrl_q[17];
-  assign s_spi_txe = s_spi_ctrl_q[16];
-  assign s_spi_div = s_spi_ctrl_q[15:8];
-  assign s_spi_dtb = s_spi_ctrl_q[7:6];
-  assign s_spi_mstr = s_spi_ctrl_q[5];
-  assign s_spi_rdm = s_spi_ctrl_q[4];
-  assign s_spi_ass = s_spi_ctrl_q[3];
-  assign s_spi_lsb = s_spi_ctrl_q[2];
-  assign s_spi_cpol = s_spi_ctrl_q[1];
-  assign s_spi_cpha = s_spi_ctrl_q[0];
+  assign apb4.pready     = 1'b1;
+  assign apb4.pslverr    = 1'b0;
 
+  assign s_bit_cpha      = s_spi_ctrl1_q[0];
+  assign s_bit_cpol      = s_spi_ctrl1_q[1];
+  assign s_bit_lsb       = s_spi_ctrl1_q[2];
+  assign s_bit_ass       = s_spi_ctrl1_q[3];
+  assign s_bit_rdm       = s_spi_ctrl1_q[4];
+  assign s_bit_sstr      = s_spi_ctrl1_q[5];
+  assign s_bit_dtb       = s_spi_ctrl1_q[7:6];
+  assign s_bit_txie      = s_spi_ctrl2_q[0];
+  assign s_bit_rxie      = s_spi_ctrl2_q[1];
+  assign s_bit_en        = s_spi_ctrl2_q[2];
+  assign s_bit_st        = s_spi_ctrl2_q[3];
+  assign s_bit_nss       = s_spi_ctrl2_q[7:4];
 
-  assign s_spi_ctrl_d    = (s_apb4_wr_hdshk && s_apb4_addr == `SPI_CTRL && ~s_spi_busy) ? apb4.pwdata : s_spi_clk_q;
-  dffr #(32) u_spi_ctrl_dffr (
+  assign s_bit_txif      = s_spi_stat_q[0];
+  assign s_bit_rxif      = s_spi_stat_q[1];
+  assign s_bit_busy      = s_spi_stat_q[2];
+  assign irq_o           = s_bit_txif | s_bit_rxif;
+
+  assign s_spi_ctrl1_en  = s_apb4_wr_hdshk && s_apb4_addr == `SPI_CTRL1 && ~s_bit_busy;
+  assign s_spi_ctrl1_d   = s_spi_ctrl1_en ? apb4.pwdata[`SPI_CTRL1_WIDTH-1:0] : s_spi_ctrl1_q;
+  dffer #(`SPI_CTRL1_WIDTH) u_spi_ctrl1_dffer (
       apb4.pclk,
       apb4.presetn,
-      s_spi_ctrl_d,
-      s_spi_ctrl_q
+      s_spi_ctrl1_en,
+      s_spi_ctrl1_d,
+      s_spi_ctrl1_q
   );
 
+  assign s_spi_ctrl2_en = s_apb4_wr_hdshk && s_apb4_addr == `SPI_CTRL2 && ~s_bit_busy;
+  assign s_spi_ctrl2_d  = s_spi_ctrl2_en ? apb4.pwdata[`SPI_CTRL2_WIDTH-1:0] : s_spi_ctrl2_q;
+  dffer #(`SPI_CTRL2_WIDTH) u_spi_ctrl2_dffer (
+      apb4.pclk,
+      apb4.presetn,
+      s_spi_ctrl2_en,
+      s_spi_ctrl2_d,
+      s_spi_ctrl2_q
+  );
+
+  assign s_spi_div_en = s_apb4_wr_hdshk && s_apb4_addr == `SPI_DIV && ~s_bit_busy;
+  assign s_spi_div_d  = s_spi_div_en ? apb4.pwdata[`SPI_DIV_WIDTH-1:0] : s_spi_div_q;
+  dffer #(`SPI_DIV_WIDTH) u_spi_div_dffer (
+      apb4.pclk,
+      apb4.presetn,
+      s_spi_div_en,
+      s_spi_div_d,
+      s_spi_div_q
+  );
+
+  assign s_spi_txr_en = s_apb4_wr_hdshk && s_apb4_addr == `SPI_TXR && ~s_bit_busy;
   always_comb begin
-    s_spi_tx_d = s_spi_tx_q;
-    if (s_apb4_wr_hdshk && s_apb4_addr == `SPI_TX && ~s_spi_busy) begin
-      unique case (s_spi_dtb)
+    s_spi_txr_d = s_spi_txr_q;
+    if (s_spi_txr_en) begin
+      unique case (s_bit_dtb)
         2'b00: s_spi_tx_d = apb4.pwdata[7:0];
         2'b01: s_spi_tx_d = apb4.pwdata[15:0];
         2'b10: s_spi_tx_d = apb4.pwdata[23:0];
@@ -69,20 +103,50 @@ module apb4_spi (
       endcase
     end
   end
-  dffr #(32) u_spi_tx_dffr (
+  dffer #(`SPI_TXR_WIDTH) u_spi_txr_dffer (
       apb4.pclk,
       apb4.presetn,
-      s_spi_tx_d,
-      s_spi_tx_q
+      s_spi_txr_en,
+      s_spi_txr_d,
+      s_spi_txr_q
   );
 
+  assign s_spi_stat_en = ((s_bit_txif || s_bit_rxif) && s_apb4_rd_hdshk && s_apb4_addr == `SPI_STAT)
+                      || (~s_bit_txif && s_bit_en && s_bit_txie && s_tx_irq_trg)
+                      || (~s_bit_rxif && s_bit_en && s_bit_rxie && s_rx_irq_trg);
+  always_comb begin
+    s_spi_stat_d = s_spi_stat_q;
+    if ((s_bit_txif || s_bit_rxif) && s_apb4_rd_hdshk && s_apb4_addr == `SPI_STAT) begin
+      s_spi_stat_d = {s_bit_busy, 2'b0};
+    end else if (~s_bit_txif && s_bit_en && s_bit_txie && s_tx_irq_trg) begin
+      s_spi_stat_d = {s_bit_busy, s_bit_rxif, 1'b1};
+    end else if (~s_bit_rxif && s_bit_en && s_bit_rxie && s_rx_irq_trg) begin
+      s_spi_stat_d = {s_bit_busy, 1'b1, s_bit_txif};
+    end else if (1'b0) begin  // TODO: busy flag
+      s_spi_stat_d = {1'b0, s_bit_rxif, s_bit_txif};
+    end
+  end
+  dffer #(`SPI_STAT_WIDTH) u_spi_stat_dffer (
+      apb4.pclk,
+      apb4.presetn,
+      s_spi_stat_en,
+      s_spi_stat_d,
+      s_spi_stat_q
+  );
+
+
+  assign s_spi_rv_rx = {s_spi_rx_q[7:0], s_spi_rx_q[15:8], s_spi_rx_q[23:16], s_spi_rx_q[31:24]};
   always_comb begin
     apb4.prdata = '0;
     if (s_apb4_rd_hdshk) begin
       unique case (s_apb4_addr)
-        `SPI_CTRL: apb4.prdata = s_spi_ctrl_q;
-        `SPI_TX:   apb4.prdata = s_spi_tx_q;
-        `SPI_RX:   apb4.pready = s_spi_rdm == 1'b1 ? s_spi_rv_rx : s_spi_rx_q;
+        `SPI_CTRL1: apb4.prdata[`SPI_CTRL1_WIDTH-1:0] = s_spi_ctrl1_q;
+        `SPI_CTRL2: apb4.prdata[`SPI_CTRL2_WIDTH-1:0] = s_spi_ctrl2_q;
+        `SPI_DIV:   apb4.prdata[`SPI_DIV_WIDTH-1:0] = s_spi_div_q;
+        `SPI_TXR:   apb4.prdata[`SPI_TXR_WIDTH-1:0] = s_spi_txr_q;
+        `SPI_RXR:   apb4.prdata[`SPI_RXR_WIDTH-1:0] = s_bit_rdm == 1'b1 ? s_spi_rv_rx : s_spi_rx_q;
+        `SPI_STAT:  apb4.prdata[`SPI_STAT_WIDTH-1:0] = s_spi_stat_q;
+        default:    apb4.prdata = '0;
       endcase
     end
   end
@@ -93,7 +157,7 @@ module apb4_spi (
       .clk_en_i       (1'b1),
       .clk_div_i      (s_spi_div),
       .clk_div_valid_i(),
-      .cpol_i         (s_spi_cpol),
+      .cpol_i         (s_bit_cpol),
       .spi_clk_o      (),
       .spi_rise_o     (),
       .spi_fall_o     ()
--- rtl/spi_define.sv
+++ rtl/spi_define.sv
@@ -12,38 +12,70 @@
 `define INC_SPI_DEF_SV
 
 /* register mapping
- * SPI_CTRL:
- * BITS:   | 31:24  | 23:21 | 20   | 19 | 18 | 17  | 16  | 15:8 | 7:6 | 5    | 4   | 3   | 2   | 1    | 0    |
- * FIELDS: | RES    | NSS   | BUSY | ST | EN | RXE | TXE | DIV  | DTB | MSTR | RDM | ASS | LSB | CPOL | CPHA |
- * PERMS:  | NONE   | RW    | RW   | RW | RW | RW  | RW  | RW   | RW  | RES  | RW  | RW  | RW  | RW   | RW   |
- * -----------------------------------------------------------------------------------------------------------
- * SPI_TX:
+ * SPI_CTRL1:
+ * BITS:   | 31:8 | 7:6 | 5    | 4   | 3   | 2   | 1    | 0    |
+ * FIELDS: | RES  | DTB | SSTR | RDM | ASS | LSB | CPOL | CPHA |
+ * PERMS:  | NONE | RW  | RW   | RW  | RW  | RW  | RW   | RW   |
+ * --------------------------------------------------------------
+* SPI_CTRL2:
+ * BITS:   | 31:8 | 7:4 | 3  | 2  | 1    | 0    |
+ * FIELDS: | RES  | NSS | ST | EN | RXIE | TXIE |
+ * PERMS:  | NONE | RW  | RW | RW | RW   | RW   |
+ * --------------------------------------------------------------
+ * SPI_DIV:
+ * BITS:   | 31:16 | 15:0 |
+ * FIELDS: | RES   | DIV  |
+ * PERMS:  | NONE  | RW   |
+ * --------------------------------------------------------------
+ * SPI_TXR:
  * BITS:   | 31:0   |
  * FIELDS: | TXDATA |
- * PERMS:  | RW     |
- * -----------------------------------------------------------------------------------------------------------
- * SPI_RX:
+ * PERMS:  | W      |
+ * --------------------------------------------------------------
+ * SPI_RXR:
  * BITS:   | 31:0   |
  * FIELDS: | RXDATA |
- * PERMS:  | RW     |
- * -----------------------------------------------------------------------------------------------------------
+ * PERMS:  | R      |
+ * ---------------------------------------------------------------
+ * SPI_STAT:
+ * BITS:   | 31:3 | 2    | 1    | 0    |
+ * FIELDS: | RES  | BUSY | RXIF | TXIF |
+ * PERMS:  | NONE | R    | R    | R    |
+ * ---------------------------------------------------------------
 */
 
 // verilog_format: off
-`define SPI_CTRL 4'b0000 // BASEADDR + 0x00
-`define SPI_TX   4'b0001 // BASEADDR + 0x04
-`define SPI_RX   4'b0010 // BASEADDR + 0x08
+`define SPI_CTRL1 4'b0000 // BASEADDR + 0x00
+`define SPI_CTRL2 4'b0001 // BASEADDR + 0x04
+`define SPI_DIV   4'b0010 // BASEADDR + 0x08
+`define SPI_TXR   4'b0011 // BASEADDR + 0x0C
+`define SPI_RXR   4'b0100 // BASEADDR + 0x10
+`define SPI_STAT  4'b0101 // BASEADDR + 0x14
+
+`define SPI_CTRL1_ADDR {26'b0, `SPI_CTRL1, 2'b00}
+`define SPI_CTRL2_ADDR {26'b0, `SPI_CTRL2, 2'b00}
+`define SPI_DIV_ADDR   {26'b0, `SPI_DIV,   2'b00}
+`define SPI_TXR_ADDR   {26'b0, `SPI_TXR,   2'b00}
+`define SPI_RXR_ADDR   {26'b0, `SPI_RXR,   2'b00}
+`define SPI_STAT_ADDR  {26'b0, `SPI_STAT,  2'b00}
+
+`define SPI_CTRL1_WIDTH 8
+`define SPI_CTRL2_WIDTH 8
+`define SPI_DIV_WIDTH   16
+`define SPI_TXR_WIDTH   32
+`define SPI_RXR_WIDTH   32
+`define SPI_STAT_WIDTH  3
+
+`define SPI_NSS_NUM 1
 
 // verilog_format: on
 
-interface spi_if #(
-    parameter int SPI_NSS_NUM = 1
-) ();
-  logic                   spi_sck_o;
-  logic [SPI_NSS_NUM-1:0] spi_nss_o;
-  logic                   spi_miso_o;
-  logic                   spi_mosi_i;
-  logic                   irq_o;
+interface spi_if ();
+  logic                    spi_sck_o;
+  logic [`SPI_NSS_NUM-1:0] spi_nss_o;
+  logic                    spi_miso_o;
+  logic                    spi_mosi_i;
+  logic                    irq_o;
 
   modport dut(
       output spi_sck_o,
